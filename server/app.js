934d055fad6492023bc333991d85e7e309a6d857
Dmitry Ryumkin <riumkin@gmail.com>
2017-08-06 13:02:33 +0300

    change security settings

--- server/app.js
+++ server/app.js
@@ -11,29 +11,14 @@ if (process.env.NODE_ENV !== 'test') {
   mongoose.connect(config.atlas);
 }
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
+  
 app.use(bodyParser.json());
   routes(app);
-
-  app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://alpha.onlineyoga.space');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 
 app.use((err, req, res, next) => {
   res.status(422).send({ error: err._message });
